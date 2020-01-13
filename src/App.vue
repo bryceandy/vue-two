920464ffcced57309661715d4057e2ffa186d24e
bryceandy <bryceandy@rocketmail.com>
2020-01-13 03:30:41 +0300

    Test a new component

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <SomeBox />
   </div>
 </template>
 
 <script>
   import HelloWorld from './components/HelloWorld.vue'
+  import SomeBox from "./components/SomeBox";
 
   export default {
     name: 'app',
     components: {
-      HelloWorld
+      HelloWorld,
+      SomeBox
     }
   }
 </script>
---
+++ src/components/SomeBox.vue
@@ -0,0 +1,26 @@
+<template>
+    <label>
+        The box
+        <input type="text" v-model="boxVal" @keyup="logBox">
+    </label>
+</template>
+
+<script>
+    export default {
+        name: "SomeBox",
+        methods: {
+            logBox () {
+                alert(this.boxVal)
+            }
+        },
+        data() {
+            return {
+                boxVal: ''
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>

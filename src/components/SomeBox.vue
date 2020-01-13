e65a2655269b77735215e09a18bbf2cface2df30
bryceandy <bryceandy@rocketmail.com>
2020-01-13 11:51:40 +0300

    Bind attribute, loop through object

--- src/components/SomeBox.vue
+++ src/components/SomeBox.vue
@@ -1,40 +1,56 @@
 <template>
-    <div class="some-box">
-        <label>
-            The box
-            <input type="text" v-model="boxVal">
-        </label>
-        <button @click="logBox">Submit</button>
-        <ul class="places">
-            <li v-bind:key="name.length" v-for="name in names" v-text="name"/>
-        </ul>
-    </div>
+  <div class="some-box">
+    <label>
+      The box
+      <input type="text" v-model="boxVal">
+    </label>
+    <button @click="logBox">Submit</button>
+    <ul class="places">
+      <li :key="index" :title="key" v-for="(item, key, index) in names" v-text="item"/>
+    </ul>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "SomeBox",
-        methods: {
-            logBox () {
-                this.names.push(this.boxVal);
-                this.boxVal = ''
-            }
-        },
-        data() {
-            return {
-                boxVal: '',
-                names: ['Arusha', 'Manyaraa', 'Ruuvuma', 'Twara']
-            }
+  export default {
+    name: "SomeBox",
+    methods: {
+      logBox () {
+
+        if (this.boxVal === 'Sinza') {
+            this.names['hoes'] = this.boxVal;
         }
+
+        if (this.boxVal === 'Gongo la Mboto') {
+            this.names['huge buses'] = this.boxVal;
+        }
+
+        this.boxVal = ''
+      }
+    },
+    data() {
+      return {
+        boxVal: '',
+        names: {
+            airport: 'Ukonga',
+            goatSales: 'Vingunguti',
+            barsEverywhere: 'Tabata',
+            market: 'Buguruni'
+        }
+      }
     }
+  }
 </script>
 
 <style scoped>
-    * {
-        font-size: large
-    }
-    button {
-        margin: 1em;
-        padding: .5em
-    }
+  * {
+    font-size: large
+  }
+  button {
+    margin: 1em;
+    padding: .5em
+  }
+  ul {
+    list-style-type: none
+  }
 </style>
